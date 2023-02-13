e099feb8ad951569b05adb5a549527508a0aa234
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2023-02-13 20:35:04 +0400

    style: modified padding for new note form

--- src/components/header-form/header-form.template.js
+++ src/components/header-form/header-form.template.js
@@ -3,7 +3,7 @@ export function getHeaderFormTemplate(data) {
     <h1 class="text-blue-400 text-2xl first-letter:text-blue-600 font-black">Notes App</h1>
     <div>
         <button class="form-btn" data-type="note-form-btn">Create Note</button>
-        <div data-type="note-form-overlay" class="bg-gray-200 bg-opacity-80 absolute inset-0 flex z-50 hidden">
+        <div data-type="note-form-overlay" class="bg-gray-200 bg-opacity-80 px-4 absolute inset-0 flex z-50 hidden">
             <form class="flex flex-col self-center mx-auto space-y-4 w-full max-w-lg bg-white rounded-md p-4">
             </form>
         </div>
