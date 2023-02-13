bb48eace508ed12cac4cae4e81a5afe4843ecbdb
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2023-02-13 14:25:34 +0400

    app state: creating store

---
+++ src/core/store.core.js
@@ -0,0 +1,22 @@
+export function createStore(rootReducer, initState = {}) {
+  let state = rootReducer({...initState}, {type: '__INIT__'});
+  let listeners = [];
+
+  return {
+    subscribe(callback) {
+      listeners.push(callback);
+      return {
+        unsubscribe() {
+          listeners = listeners.filter((listener) => listener !== callback);
+        },
+      };
+    },
+    dispatch(action) {
+      state = rootReducer(state, action);
+      listeners.forEach((listener) => listener(state));
+    },
+    getState() {
+      return JSON.parse(JSON.stringify(state));
+    },
+  };
+}
--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import './styles/index.css';
 import {App} from './components/app/app.component';
 import {HeaderForm} from './components/header-form/header-form.component';
 import {NotesList} from './components/notes-list/notes-list.component';
+import {createStore} from './core/store.core';
+import {rootReducer} from './store/rootReducer';
 
+const store = createStore(rootReducer);
 const app = new App('#root', {
   components: [HeaderForm, NotesList],
+  store,
 });
 
 app.render();
