f5d0680732185563c6a4d5d2e8bde09a878c0a6f
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2023-02-15 18:06:40 +0400

    style: modified notes styling

--- src/components/notes-list/notes-list.component.js
+++ src/components/notes-list/notes-list.component.js
@@ -61,8 +61,12 @@ export class NotesList extends AppComponent {
       notesToRender
           .map(
               ({title='', description='', priority=0, id=''}) =>
-                `<div class="px-2 md:px-4 py-6">
-                  <div class="relative px-4 pb-2 pt-8 rounded-lg shadow-lg bg-white break-words word">
+                `<div class="app-note-wrapper">
+                  <div class="app-note">
+                      <div class="flex justify-between w-full space-x-2 mb-4">
+                        <button class="edit-btn" data-id="${id}" data-type="note-edit-btn">Edit</button>
+                        <button class="delete-btn" data-id="${id}" data-type="note-delete-btn">x</button>
+                      </div>
                       <h5 class="text-gray-900 text-xl leading-tight font-medium mb-2">${title}</h5>
                       <p class="text-gray-700 text-base mb-4">
                         ${description}
@@ -70,10 +74,6 @@ export class NotesList extends AppComponent {
                       <p class="text-gray-700 text-sm text-right">
                         Priority: <span class="font-semibold" style="color: ${this.getPriorityColor(priority)}">${priority}</span>
                       </p>
-                      <div class="flex justify-between w-full space-x-2 px-4 absolute -top-1 left-0">
-                          <button class="edit-btn" data-id="${id}" data-type="note-edit-btn">Edit</button>
-                          <button class="delete-btn" data-id="${id}" data-type="note-delete-btn">x</button>
-                      </div>
                   </div>
                 </div>
             `)
