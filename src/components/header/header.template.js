27e5c4216996cd7ba8d1e016bc1ceb6a4b423d5f
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2023-02-16 20:58:59 +0400

    style: header buttons responsive layout

--- src/components/header/header.template.js
+++ src/components/header/header.template.js
@@ -1,7 +1,7 @@
 export function getHeaderTemplate() {
   return `
     <h1 class="text-blue-400 text-2xl first-letter:text-blue-600 font-black">Notes App</h1>
-    <div class="flex space-x-2">
+    <div class="flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-2">
       <button class="form-delete-btn hidden" data-type="note-delete-btn">Delete Selected</button>
       <button class="form-primary-btn" data-type="note-form-btn">Create Note</button>
     </div>
